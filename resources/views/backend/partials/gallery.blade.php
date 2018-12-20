14d2d8edd15f481c057da017d04e00ac97a40dc7
uusa35 <uusa35@gmail.com>
2018-12-20 17:22:53 +0300

    updates

--- resources/views/backend/partials/gallery.blade.php
+++ resources/views/backend/partials/gallery.blade.php
@@ -1,6 +1,7 @@
 <div class="portfolio-content portfolio-1">
     <div id="js-filters-juicy-projects" class="cbp-l-filters-button">
-        <div data-filter="*" class="cbp-filter-item-active cbp-filter-item btn dark btn-outline uppercase"> {{ trans('general.all_images') }}
+        <div data-filter="*"
+             class="cbp-filter-item-active cbp-filter-item btn dark btn-outline uppercase"> {{ trans('general.all_images') }}
             <div class="cbp-filter-counter"></div>
         </div>
         {{--<div data-filter=".identity" class="cbp-filter-item btn dark btn-outline uppercase"> Identity--}}
@@ -18,15 +19,15 @@
                             <div class="cbp-l-caption-body">
                                 <a href="{{ asset(env('LARGE').$img->image) }}"
                                    class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase"
-                                   data-title="Dashboard<br>by Paul Flavius Nechita">{{ trans('general.view_larger') }}</a>
+                                   data-title="{{  $element->caption }} <br> {{ $element->notes  }}">{{ trans('general.view_larger') }}</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="cbp-l-grid-projects-title uppercase text-center uppercase text-center">{{ $img->name }}</div>
-                <div class="cbp-l-grid-projects-desc uppercase text-center uppercase text-center">{{ $img->caption }}
-                    Graphic
-                </div>
+                <div
+                    class="cbp-l-grid-projects-title uppercase text-center uppercase text-center">{{ $img->name }}</div>
+                <div
+                    class="cbp-l-grid-projects-desc uppercase text-center uppercase text-center">{{ $img->caption }}</div>
             </div>
         @endforeach
     </div>
