5551bd1f71d8f384c9b7dfed8bf1f6aa77dffdd8
uusa35 <uusa35@gmail.com>
2019-01-03 19:03:33 +0300

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,12 +92,13 @@
         }
         .links > a {
             color: #636b6f;
-            padding: 0 25px;
+            /*padding: 0 25px;*/
+            margin-left: 15px;
+            margin-right: 15px;
             font-size: 13px;
             font-weight: 600;
-            letter-spacing: .1rem;
             text-decoration: none;
-            text-transform: uppercase;
+            /*text-transform: uppercase;*/
         }
 
         .m-b-md {
@@ -138,19 +139,19 @@
             <div class="links">
                 @auth
                     @can('isAdmin')
-                        <a href="{{ route('backend.home') }}">{{ trans('general.dashboard') }}</a>
-                        <a href="{{ route('backend.admin.order.index') }}">{{ trans('general.orders') }}</a>
-                        <a href="{{ route('backend.admin.setting.index') }}">{{ trans('general.settings') }}</a>
-                        <a href="{{ route('backend.admin.category.index') }}">{{ trans('general.categories') }}</a>
-                        <a href="{{ route('backend.admin.service.index') }}">{{ trans('general.services') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.home') }}" role="button"> {{ trans('general.dashboard') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.order.index') }}">{{ trans('general.orders') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.setting.index') }}">{{ trans('general.settings') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.category.index') }}">{{ trans('general.categories') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.service.index') }}">{{ trans('general.services') }}</a>
                     @elsecan('onlyDesigner')
-                        <a href="{{ route('backend.order.index') }}">Orders & {{ trans('general.my_jobs') }}</a>
-                        <a href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.order.index') }}">Orders & {{ trans('general.my_jobs') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
                     @elsecan('onlyClient')
-                        <a href="{{ route('backend.order.index') }}">My {{ trans('general.orders') }}</a>
-                        <a href="{{ route('backend.point.index') }}">{{ trans('general.recharge') }}</a>
-                        <a href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
-                        <a href="{{ route('backend.file.show', auth()->id()) }}">My {{ trans('general.my_files') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.order.index') }}">My {{ trans('general.orders') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.point.index') }}">{{ trans('general.recharge') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.file.show', auth()->id()) }}">My {{ trans('general.my_files') }}</a>
                     @endif
                 @endauth
             </div>
