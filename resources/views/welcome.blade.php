cd7fe256f1f570b4ec5f3f61b2f1f417764a830e
uusa35 <uusa35@gmail.com>
2019-01-30 13:17:35 +0300

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,6 +90,7 @@
             margin: 25px;
             text-align: center;
         }
+
         .links > a {
             color: #636b6f;
             /*padding: 0 25px;*/
@@ -108,50 +109,105 @@
 </head>
 <body>
 <div class="container-fluid">
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ route('home') }}">{{ trans('general.home') }}</a>
-                    <a href="{{ url('/logout') }}"
-                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                        <i class="icon-key"></i> {{ trans('general.logout') }} </a>
-                @else
-                    <a href="{{ route('login') }}">{{ trans('general.login') }}</a>
+    <div class="row">
+        <div class="col-sm">
+            @guest
+                <div class="links">
+                    <form class="form-inline" method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group mb-4">
+                            <label for="staticEmail2" class="sr-only">Email</label>
+                            <input id="email" type="email"
+                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                   name="email" value="{{ old('email') }}" required autofocus>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                            @endif
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">{{ trans('general.register') }}</a>
-                    @endif
-                @endauth
-                    <a href="{{ route('frontend.language.change',app()->isLocale('ar') ? 'en' : 'ar') }}">{{ trans('general.switch_lang') }}</a>
-            </div>
-        @endif
-
-        <div class="content">
+                        </div>
+                        <div class="form-group mx-sm-3 mb-4">
+                            <label for="inputPassword2" class="sr-only">Password</label>
+                            <input id="password" type="password"
+                                   class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                   name="password" required>
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-3">{{ trans('general.login') }}</button>
+                    </form>
+                </div>
+            @endif
+        </div>
+        <div class="col-sm">
             <div class="title m-b-md text-center">
                 <img style="max-width: 120px;" src="{{ asset(env('THUMBNAIL').$settings->logo) }}"
                      alt="{{ $settings->name }}">
             </div>
-            <p class="text-center">
-                {{ $settings->on_home_speech }}
-            </p>
+        </div>
+        <div class="col-sm">
+            @if (Route::has('login'))
+                <div class="links">
+                    @auth
+                        <a href="{{ route('home') }}">{{ trans('general.home') }}</a>
+                        <a href="{{ url('/logout') }}"
+                           onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            <i class="icon-key"></i> {{ trans('general.logout') }} </a>
+                    @else
+                        {{--                        <a href="{{ route('login') }}">{{ trans('general.login') }}</a>--}}
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">{{ trans('general.register') }}</a>
+                        @endif
+                    @endauth
+                    {{--<a href="{{ route('frontend.language.change',app()->isLocale('ar') ? 'en' : 'ar') }}">{{ trans('general.switch_lang') }}</a>--}}
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="flex-center position-ref full-height">
+
+        <div class="content">
+            {{--<div class="row">--}}
+                {{--<div class="col">--}}
+                    {{--<p class="text-center">--}}
+                        {{--{{ $settings->on_home_speech }}--}}
+                    {{--</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
             <div class="links">
                 @auth
                     @can('isAdmin')
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.home') }}" role="button"> {{ trans('general.dashboard') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.order.index') }}">{{ trans('general.orders') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.setting.index') }}">{{ trans('general.settings') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.category.index') }}">{{ trans('general.categories') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.admin.service.index') }}">{{ trans('general.services') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.home') }}"
+                           role="button"> {{ trans('general.dashboard') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.admin.order.index') }}">{{ trans('general.orders') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.admin.setting.index') }}">{{ trans('general.settings') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.admin.category.index') }}">{{ trans('general.categories') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.admin.service.index') }}">{{ trans('general.services') }}</a>
                     @elsecan('onlyDesigner')
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.order.index') }}">Orders & {{ trans('general.my_jobs') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
+                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.order.index') }}">Orders
+                            & {{ trans('general.my_jobs') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
                     @elsecan('onlyClient')
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.order.index') }}">My {{ trans('general.orders') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.point.index') }}">{{ trans('general.recharge') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
-                        <a class="btn btn-outline-light btn-lg" href="{{ route('backend.file.show', auth()->id()) }}">My {{ trans('general.my_files') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.order.index') }}">My {{ trans('general.orders') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.point.index') }}">{{ trans('general.recharge') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.user.show', auth()->id()) }}">My {{ trans('general.my_profile') }}</a>
+                        <a class="btn btn-outline-light btn-lg"
+                           href="{{ route('backend.file.show', auth()->id()) }}">My {{ trans('general.my_files') }}</a>
                     @endif
                 @endauth
             </div>
