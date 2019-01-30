51a33ccb77436dfcfb9760cd629adf5f1ff02c05
uusa35 <uusa35@gmail.com>
2019-01-30 14:40:23 +0300

    updates

--- resources/views/backend/partials/_comments.blade.php
+++ resources/views/backend/partials/_comments.blade.php
@@ -1,36 +1,36 @@
-<div class="row">
-    <div class="col-lg-12">
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                {{ trans("general.comments") }}
-            </div>
-            <div class="panel-body">
-                <div id="disqus_thread"></div>
-                @section('scripts')
-                    @parent
-                    <script>
-                        /**
-                         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-                        /*
-                        var disqus_config = function () {
-                        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-                        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                        };
-                        */
-                        (function() { // DON'T EDIT BELOW THIS LINE
-                            var d = document, s = d.createElement('script');
-                            s.src = 'https://points-sys.disqus.com/embed.js';
-                            s.setAttribute('data-timestamp', +new Date());
-                            (d.head || d.body).appendChild(s);
-                        })();
-                    </script>
-                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments
-                            powered
-                            by Disqus.</a></noscript>
-                @endsection
-            </div>
-        </div>
-    </div>
-</div>
+{{--<div class="row">--}}
+    {{--<div class="col-lg-12">--}}
+        {{--<div class="panel panel-info">--}}
+            {{--<div class="panel-heading">--}}
+                {{--{{ trans("general.comments") }}--}}
+            {{--</div>--}}
+            {{--<div class="panel-body">--}}
+                {{--<div id="disqus_thread"></div>--}}
+                {{--@section('scripts')--}}
+                    {{--@parent--}}
+                    {{--<script>--}}
+                        {{--/**--}}
+                         {{--*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.--}}
+                         {{--*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/--}}
+                        {{--/*--}}
+                        {{--var disqus_config = function () {--}}
+                        {{--this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable--}}
+                        {{--this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable--}}
+                        {{--};--}}
+                        {{--*/--}}
+                        {{--(function() { // DON'T EDIT BELOW THIS LINE--}}
+                            {{--var d = document, s = d.createElement('script');--}}
+                            {{--s.src = 'https://points-sys.disqus.com/embed.js';--}}
+                            {{--s.setAttribute('data-timestamp', +new Date());--}}
+                            {{--(d.head || d.body).appendChild(s);--}}
+                        {{--})();--}}
+                    {{--</script>--}}
+                    {{--<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments--}}
+                            {{--powered--}}
+                            {{--by Disqus.</a></noscript>--}}
+                {{--@endsection--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 
